4b66693ee1920507a31827ac003c4f9f3c106ce3
Jonas Seidl <js408@hdm-stuttgart.de>
2021-11-02 10:52:57 +0100

    Cleaned up the code a bit

--- Nodes/Game Objects/MusicController.gd
+++ Nodes/Game Objects/MusicController.gd
@@ -18,6 +18,9 @@ var volume_change : float = 0.0
 
 var switching : bool = false
 
+onready var node_calm_music : AudioStreamPlayer = $Calm_music
+onready var node_hectic_music : AudioStreamPlayer = $Hectic_music
+
 
 func _ready():
 	if is_active:
@@ -26,19 +29,19 @@ func _ready():
 		var calm_audio_stream = load(calm_music_path)
 		var hectic_audio_stream = load(hectic_music_path)
 		
-		$Calm_music.stream = calm_audio_stream
-		$Hectic_music.stream = hectic_audio_stream
+		node_calm_music.stream = calm_audio_stream
+		node_hectic_music.stream = hectic_audio_stream
 		
 		# initializing the tracks
-		$Calm_music.play()
-		$Hectic_music.play()
+		node_calm_music.play()
+		node_hectic_music.play()
 		
 		if starting_track == SoundtrackTypes.MUSIC_CALM:
-			$Hectic_music.stream_paused = true
-			$Hectic_music.volume_db = -VOLUME_RANGE
+			node_hectic_music.stream_paused = true
+			node_hectic_music.volume_db = -VOLUME_RANGE
 		elif starting_track == SoundtrackTypes.MUSIC_HECTIC:
-			$Calm_music.stream_paused = true
-			$Calm_music.volume_db = -VOLUME_RANGE
+			node_calm_music.stream_paused = true
+			node_calm_music.volume_db = -VOLUME_RANGE
 
 
 func _process(delta):
@@ -47,7 +50,7 @@ func _process(delta):
 		switch_soundtracks()
 
 
-func track_player():
+func track_player() -> void:
 	
 	# track the players movement and calculate the volume adjustement based on it
 	if player != null:
@@ -56,25 +59,26 @@ func track_player():
 		
 		var volume_adjustement = (player_distance / transition_area_width) * VOLUME_RANGE
 		
-		if previous_active_music == $Calm_music:
-			tracking_adjust_volume($Calm_music, $Hectic_music, volume_adjustement)
-		elif previous_active_music == $Hectic_music:
-			tracking_adjust_volume($Hectic_music, $Calm_music, volume_adjustement)
+		if previous_active_music == node_calm_music:
+			tracking_adjust_volume(node_calm_music, node_hectic_music, volume_adjustement)
+		elif previous_active_music == node_hectic_music:
+			tracking_adjust_volume(node_hectic_music, node_calm_music, volume_adjustement)
 
 
 func start_tracking(body: Player, music_transition_area: MusicTransitionArea) -> void:
 	# init tracking data
 	player = body
 	player_last_position = body.position.x
-	transition_area_width = (music_transition_area.scale.x * 2) + (body.get_node("CollisionShape2D").get_shape().extents.x * 2)
+	transition_area_width = ((music_transition_area.scale.x * 2) + 
+			(body.get_node("CollisionShape2D").get_shape().extents.x * 2))
 	
 	# start the paused Soundtrack
-	if $Hectic_music.stream_paused:
-		$Hectic_music.stream_paused = false
-		previous_active_music = $Calm_music
-	elif $Calm_music.stream_paused:
-		$Calm_music.stream_paused = false
-		previous_active_music = $Hectic_music
+	if node_hectic_music.stream_paused:
+		node_hectic_music.stream_paused = false
+		previous_active_music = node_calm_music
+	elif node_calm_music.stream_paused:
+		node_calm_music.stream_paused = false
+		previous_active_music = node_hectic_music
 
 
 func stop_tracking() -> void:
@@ -82,64 +86,67 @@ func stop_tracking() -> void:
 	volume_change = 0.0
 	
 	# reset the volumes and pause the not wanted soundtrack
-	if $Calm_music.volume_db > $Hectic_music.volume_db:
-		$Calm_music.volume_db = 0
-		$Hectic_music.volume_db = -VOLUME_RANGE
-		$Hectic_music.stream_paused = true
-	elif $Hectic_music.volume_db > $Calm_music.volume_db:
-		$Hectic_music.volume_db = 0
-		$Calm_music.volume_db = -VOLUME_RANGE
-		$Calm_music.stream_paused = true
+	if node_calm_music.volume_db > node_hectic_music.volume_db:
+		node_calm_music.volume_db = 0
+		node_hectic_music.volume_db = -VOLUME_RANGE
+		node_hectic_music.stream_paused = true
+	elif node_hectic_music.volume_db > node_calm_music.volume_db:
+		node_hectic_music.volume_db = 0
+		node_calm_music.volume_db = -VOLUME_RANGE
+		node_calm_music.stream_paused = true
 
 
-func tracking_adjust_volume(music_old: AudioStreamPlayer, music_new: AudioStreamPlayer, volume_adjustement: float) -> void:
+func tracking_adjust_volume(music_old: AudioStreamPlayer, music_new: AudioStreamPlayer,
+		volume_adjustement: float) -> void:
 	
 	volume_change += volume_adjustement
 	
-	# adjust the volumes, but with nonlinearity
-	music_old.volume_db = - (pow(abs(volume_change) / VOLUME_RANGE, interpolation_damper) * VOLUME_RANGE)
-	music_new.volume_db = - ((pow((VOLUME_RANGE - abs(volume_change)) / VOLUME_RANGE, interpolation_damper) * VOLUME_RANGE))
+	# adjust the volumes, but with non-linearity
+	music_old.volume_db = - (pow(abs(volume_change) / VOLUME_RANGE,
+			interpolation_damper) * VOLUME_RANGE)
+	music_new.volume_db = - ((pow((VOLUME_RANGE - abs(volume_change)) / VOLUME_RANGE,
+			interpolation_damper) * VOLUME_RANGE))
 
 
 # load another soundtrack
-func change_soundtrack(type, track_path: String) -> void:
+func change_soundtrack(soundtrack_type, track_path: String) -> void:
 	var audio_stream = load(track_path)
 	
-	if type == SoundtrackTypes.MUSIC_HECTIC:
-		$Hectic_music.stop()
-		$Hectic_music.stream = audio_stream
-		$Hectic_music.play()
-	elif type == SoundtrackTypes.MUSIC_CALM:
-		$Calm_music.stop()
-		$Calm_music.stream = audio_stream
-		$Calm_music.play()
+	if soundtrack_type == SoundtrackTypes.MUSIC_HECTIC:
+		node_hectic_music.stop()
+		node_hectic_music.stream = audio_stream
+		node_hectic_music.play()
+	elif soundtrack_type == SoundtrackTypes.MUSIC_CALM:
+		node_calm_music.stop()
+		node_calm_music.stream = audio_stream
+		node_calm_music.play()
 
 
-func set_switching():
+func set_switching() -> void:
 	switching = true
 	
-	if $Hectic_music.stream_paused:
-		$Hectic_music.stream_paused = false
-		previous_active_music = $Calm_music
-	elif $Calm_music.stream_paused:
-		$Calm_music.stream_paused = false
-		previous_active_music = $Hectic_music
+	if node_hectic_music.stream_paused:
+		node_hectic_music.stream_paused = false
+		previous_active_music = node_calm_music
+	elif node_calm_music.stream_paused:
+		node_calm_music.stream_paused = false
+		previous_active_music = node_hectic_music
 
 
 # Switch soundtracks with simple fade in and fade out
 func switch_soundtracks() -> void:
 	if switching:
 		
-		if previous_active_music == $Calm_music:
-			$Calm_music.volume_db -= 2
-			$Hectic_music.volume_db += 2
-			if $Hectic_music.volume_db == 0:
+		if previous_active_music == node_calm_music:
+			node_calm_music.volume_db -= 2
+			node_hectic_music.volume_db += 2
+			if node_hectic_music.volume_db == 0:
 				switching = false
-				$Calm_music.stream_paused = true
+				node_calm_music.stream_paused = true
 				
-		elif previous_active_music == $Hectic_music:
-			$Hectic_music.volume_db -= 2
-			$Calm_music.volume_db += 2
-			if $Calm_music.volume_db == 0:
+		elif previous_active_music == node_hectic_music:
+			node_hectic_music.volume_db -= 2
+			node_calm_music.volume_db += 2
+			if node_calm_music.volume_db == 0:
 				switching = false
-				$Hectic_music.stream_paused = true
+				node_hectic_music.stream_paused = true
--- Nodes/Game Objects/MusicTransitionArea.gd
+++ Nodes/Game Objects/MusicTransitionArea.gd
@@ -1,13 +1,20 @@
 class_name MusicTransitionArea
 extends Area2D
 
+enum TransitionTypes {PLAYER_TRACKING, FADE}
+
+export(TransitionTypes) var transition_type = TransitionTypes.PLAYER_TRACKING
+
 
 func _on_MusicTransitionArea_body_entered(body):
-	MusicController.start_tracking(body, self)
-	#MusicController.change_soundtrack(MusicController.SoundtrackTypes.MUSIC_CALM, "res://Nodes/Experimental/Placeholder/Sea_levelhectic_Demo_1.mp3")
-	#MusicController.set_switching()
+	if transition_type == TransitionTypes.PLAYER_TRACKING:
+		MusicController.start_tracking(body, self)
+	elif transition_type == TransitionTypes.FADE:
+		MusicController.set_switching()
+	#MusicController.change_soundtrack(MusicController.SoundtrackTypes.MUSIC_CALM, 
+	#		"res://Nodes/Experimental/Placeholder/Sea_levelhectic_Demo_1.mp3")
 
 
 func _on_MusicTransitionArea_body_exited(body):
-	MusicController.stop_tracking()
-	#pass
+	if transition_type == TransitionTypes.PLAYER_TRACKING:
+		MusicController.stop_tracking()
