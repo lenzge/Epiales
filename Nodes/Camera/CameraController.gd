7e4760df7c4abb4031638bfdc93a4051630734f9
Marian Syska <marian.syska@web.de>
2021-11-21 16:59:51 +0100

    fix camera bug

--- Nodes/Camera/CameraController.gd
+++ Nodes/Camera/CameraController.gd
@@ -14,9 +14,7 @@ export var camera_speed = 500.0
 export var reverse_drag = 0.75
 export var destination = Vector2(0, 0)
 export var side_margin = 50
-
-onready var parent = get_parent()
-
+export var player_path : NodePath
 
 # Raycast Variables to define camera collision limits
 export var raycast_down_cast_to = Vector2(0, 100)
@@ -26,13 +24,10 @@ export var raycast_right_cast_to = Vector2(100, 0)
 onready var raycast_down = get_node("RayCast2D_Down")
 onready var raycast_left = get_node("RayCast2D_Left")
 onready var raycast_right = get_node("RayCast2D_Right")
-
+onready var player = get_node(player_path)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_as_toplevel(true)
-	position = parent.position
-
 	raycast_down.set_cast_to(raycast_down_cast_to)
 	raycast_left.set_cast_to(raycast_left_cast_to)
 	raycast_right.set_cast_to(raycast_right_cast_to)
@@ -43,7 +38,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	
 	if Input.is_action_pressed("debug"):
 		if current_mode == Mode.STATIC:
@@ -52,18 +47,12 @@ func _process(delta):
 			current_mode = Mode.STATIC
 
 	match current_mode:
-		Mode.STATIC:
-			destination = position
 		Mode.FOLLOW:
-			var reverse_drag_vector = Vector2(0, 0) #if parent.velocity && parent.velocity.x == 0 else Vector2(parent.velocity.x, 0.0) * reverse_drag 
-			destination = parent.position + reverse_drag_vector
+			var reverse_drag_vector = Vector2(0, 0) if player.velocity && player.velocity.x == 0 else Vector2(player.velocity.x, 0.0) * reverse_drag 
+			destination = player.global_position + reverse_drag_vector
 			#position = position.move_toward(parent.position + reverse_drag_vector, camera_speed * delta)
 		Mode.CUTSCENE:
 			pass
-	
-	raycast_down.position = position
-	raycast_left.position = position
-	raycast_right.position = position
 	
 	if raycast_down.is_colliding():
 		print(raycast_down, raycast_down.get_collision_point(), raycast_down.get_cast_to(), raycast_down.get_collider().name, " I am colliding down!")
@@ -76,13 +65,15 @@ func _process(delta):
 	if raycast_right.is_colliding():
 		print(raycast_right, raycast_right.get_collision_point(), raycast_right.get_cast_to(), raycast_right.get_collider().name, " I am colliding right!")
 		destination.x = min(destination.x, raycast_right.get_collision_point().x + side_margin)
-
+	
 	# Move the camera towards the destination
 	if destination.round() != position.round():
-		position = position.move_toward(destination, camera_speed * delta)			
-			
+		global_position = global_position.move_toward(destination, camera_speed * delta)
+
+
 func set_destination(var new_destination):
 	destination = new_destination
-	
+
+
 func set_mode(var new_mode):
 	current_mode = new_mode
