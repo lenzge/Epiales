8827e133c9bc0db837091ebf70aad40183acbfaf
Jonas Seidl <js408@hdm-stuttgart.de>
2021-12-11 20:47:02 +0100

    Added the dash resetter sound

--- Nodes/Game Objects/DashResetter.gd
+++ Nodes/Game Objects/DashResetter.gd
@@ -1,5 +1,7 @@
 extends DamageReceiver
 
+onready var sound_machine : SoundMachine = $SoundMachine
+
 export (int) var durability = 10
 
 func _ready():
@@ -16,5 +18,6 @@ func reduce_durability(dmg):
 	if(durability <= 0):
 		owner.player_instance.can_dash = true
 		self.visible = false
+		sound_machine.play_sound("Orb", false)
 #		owner.remove_child(self) #leave inside child tree in case of a level reset
 #		self.queue_free() #leave inside child tree in case of a level reset
