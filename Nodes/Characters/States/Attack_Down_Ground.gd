3d6c2a2943464f89c3960bb20fca40c307a6ab2e
Jonas Seidl <js408@hdm-stuttgart.de>
2021-12-14 14:11:53 +0100

    fixed bug that the first up and down attacks on ground don't deal any damage

--- Nodes/Characters/States/Attack_Down_Ground.gd
+++ Nodes/Characters/States/Attack_Down_Ground.gd
@@ -11,6 +11,10 @@ func enter(msg :={}):
 	timer.start()
 	# enable the attack hitboxes
 	player.get_node("Attack_Down_Ground/HitboxAttack").disabled = false
+	
+	#TODO: knockback_force should be 0, but then the enemy gets no damage...
+	player.hitbox_down_attack.knockback_force = player.attack_force[0]
+	player.hitbox_down_attack.knockback_time = player.attack_knockback[0]
 
 
 func exit():
@@ -20,9 +24,6 @@ func exit():
 
 func _on_timeout():
 	var input = player.pop_combat_queue()
-	#TODO: knockback_force should be 0, but then the enemy gets no damage...
-	player.hitbox_down_attack.knockback_force = player.attack_force[0]
-	player.hitbox_down_attack.knockback_time = player.attack_knockback[0]
 	state_machine.transition_to("Attack_Down_Ground_Recovery")
 
 
