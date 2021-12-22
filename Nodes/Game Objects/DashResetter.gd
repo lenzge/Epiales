0ac776fc7fba8ffff6565e8d03097be2cabaac8e
Marco de Jesus Antonio <md131@hdm-stuttgart.de>
2021-12-22 21:53:45 +0100

    small Level adjustments

--- Nodes/Game Objects/DashResetter.gd
+++ Nodes/Game Objects/DashResetter.gd
@@ -18,7 +18,7 @@ func reduce_durability(dmg):
 	durability -= dmg
 	if(is_active and durability <= 0):
 		owner.player_instance.can_dash = true
-		is_active = false
+#		is_active = false
 #		self.visible = false
 		sound_machine.play_sound("Orb", false)
 #		owner.remove_child(self) #leave inside child tree in case of a level reset
