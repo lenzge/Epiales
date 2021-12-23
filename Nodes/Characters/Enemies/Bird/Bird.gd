c4638d7914cea76f89c773c03484a7122a6bc137
Marian Syska <marian.syska@web.de>
2021-12-23 01:09:09 +0100

    threw out bird

--- Nodes/Characters/Enemies/Bird/Bird.gd
+++
@@ -1,9 +0,0 @@
-extends Character
-
-func on_hit(emitter : DamageEmitter):
-	var direction
-	if emitter.direction.is_equal_approx(Vector2.ZERO):
-		direction = (hitbox.global_position - emitter.global_position).normalized() 
-	else:
-		direction = emitter.direction
-	state_machine.transition_to("Flinch", {"direction": direction, "force": emitter.knockback_force})
--- Nodes/Characters/Enemies/Bird/States/BirdFlinch.gd
+++
@@ -1,4 +0,0 @@
-extends "res://Nodes/Characters/Enemies/States/Flinch.gd"
-
-func _on_timeout():
-	state_machine.transition_to("Fly")
