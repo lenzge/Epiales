b6878af3d25015d79e5fe1201d1e5a0580574f76
Jonas Seidl <js408@hdm-stuttgart.de>
2022-01-14 15:52:08 +0100

    Added Attack grunts for Luci

--- Nodes/Characters/States/Attack_Down_Ground.gd
+++ Nodes/Characters/States/Attack_Down_Ground.gd
@@ -12,6 +12,7 @@ func enter(msg :={}):
 	player.hitbox_down_attack.knockback_time = player.attack_knockback[0]
 	
 	player.sound_machine.play_sound("Sword Swing " + str(player.sound_machine.get_random(1, 2)), false)
+	player.sound_machine.play_sound("Attack Grunt " + str(player.sound_machine.get_random(1, 8)), false)
 
 
 func exit():
