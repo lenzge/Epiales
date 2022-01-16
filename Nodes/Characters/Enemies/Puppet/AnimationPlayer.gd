fc0af67d954efff21434551d6865ea28da8a8a49
Musse Lena <lm138@hdm-stuttgart.de>
2022-01-16 13:31:58 +0100

    Add Puppet animations WIP

--- Nodes/Characters/Enemies/Puppet/AnimationPlayer.gd
+++ Nodes/Characters/Enemies/Puppet/AnimationPlayer.gd
@@ -4,6 +4,8 @@ extends AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_set_next("Run_Start", "Run_Loop")
-
+	animation_set_next("Walk_Start", "Walk_Loop")
+	animation_set_next("Run_End", "Idle")
+	animation_set_next("Walk_End", "Idle")
 
 
