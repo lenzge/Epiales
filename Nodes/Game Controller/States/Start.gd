ea8534ba12cab6e9563b7936797cb958225f39fa
Musse Lena <lm138@hdm-stuttgart.de>
2022-01-16 16:11:19 +0100

    Add animated title screen

--- Nodes/Game Controller/States/Start.gd
+++ Nodes/Game Controller/States/Start.gd
@@ -4,19 +4,17 @@ var startscreen
 
 # Called by the state machine upon changing the active state
 func enter(_msg := {}):
-	#placeholder startscreen
-	startscreen = Panel.new()
-	var message = Label.new()
-	message.text = "Press ENTER to start!"
-	startscreen.set_size(Vector2(300,50))
-	startscreen.add_child(message)
-	startscreen.set_position(OS.get_real_window_size() / 2)
+	startscreen = preload("res://Nodes/GUI/StartScreen.tscn").instance()
+	startscreen.get_child(1).set_position(OS.get_real_window_size() / 2)
 	game.add_child(startscreen)
 
 # Corresponds to the `_physics_process()` callback
 func physics_update(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
+		startscreen.animationPlayer.play("Close")
+		yield(startscreen, "close_finished")
 		state_machine.transition_to("Ingame")
+	
 
 # Called by the state machine before changing the active state (clean up)
 func exit():
