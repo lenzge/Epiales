02cf9ee4ce331d541df9aa0b3d01a9358ba41ff5
Geupel Simon <sg184@hdm-stuttgart.de>
2022-01-21 13:15:46 +0100

    Finished Charged Dash

--- Nodes/Characters/States/Dash.gd
+++ Nodes/Characters/States/Dash.gd
@@ -3,6 +3,7 @@ extends PlayerState
 var _dash_direction := Vector2()
 var _jumped := false
 onready var charged_hitbox : DamageEmitter = $"../../Charged_Dash"
+var charged_timer : Timer
 
 
 func enter(_msg := {}):
@@ -10,7 +11,6 @@ func enter(_msg := {}):
 	timer.start(player.dash_time)
 	player.can_dash = false
 	player.can_reset_dash = false
-	player.hitbox.get_child(0).disabled = true
 	
 	_dash_direction.x = -Input.get_action_strength("move_left") + Input.get_action_strength("move_right")
 	_dash_direction.y = -Input.get_action_strength("move_up") + Input.get_action_strength("move_down")
@@ -19,46 +19,65 @@ func enter(_msg := {}):
 	if player.is_on_floor() and is_equal_approx(_dash_direction.x, 0) and _dash_direction.y > 0:
 		# Edge case: Dash while crouching --> still dash foreward
 		_dash_direction = Vector2.ZERO
-	if (_dash_direction.x == 0 and _dash_direction.y == 0): # Dashing without steering in a _dash_direction
+	# Dashing without steering in a _dash_direction
+	# or Charged dash <- only possible on ground
+	if (_dash_direction.x == 0 and _dash_direction.y == 0 or
+		Input.is_action_pressed("charge") and player.is_on_floor() and player.charge_controller.has_charge()):
 		if player.sprite.flip_h:
 			_dash_direction.x = -1
 		else:
 			_dash_direction.x = 1
+		_dash_direction.y = 0
 	_dash_direction = _dash_direction.normalized()
 	
-	if Input.is_action_pressed("charge"):# and player.charge_controller.has_charge():
+	# Charged Dash
+	if Input.is_action_pressed("charge") and player.is_on_floor() and player.charge_controller.has_charge():
 		player.charge()
-		timer.start(player.charged_dash_time)
 		animationPlayer.play("Dash_Charged")
-		#animationPlayer.set_speed_scale(animationPlayer.current_animation_length/player.charged_dash_time) # Animation takes as long as charged_dash_time
-		player.velocity = _dash_direction * player.dash_speed * 0.9 # Make Charged Dash a little slower but longer
+		timer.start(animationPlayer.current_animation_length)
 		charged_hitbox.knockback_force = player.charged_dash_damage
 		charged_hitbox.emitter_path = player.get_path() # todo change this to velocity?
 		charged_hitbox.direction = _dash_direction
+		# Set Player Speed after initial animaiton
+		charged_timer = Timer.new()
+		charged_timer.set_one_shot(true)
+		charged_timer.connect("timeout", self, "set_velocity")
+		add_child(charged_timer)
+		charged_timer.start(1)
+		
+	# Normal dash
 	else:
 		timer.start(player.dash_time)
 		animationPlayer.play("Dash")
 		animationPlayer.set_speed_scale(animationPlayer.current_animation_length/player.dash_time) # Animation takes as long as dash_time
 		player.velocity = _dash_direction * player.dash_speed
+		player.hitbox.get_child(0).disabled = true
+		
+		# Tilt in _dash_direction (Attention to crouch and dash)
+		if not (_dash_direction.y > 0.5 and player.is_on_floor()):
+			player.sprite.set_rotation_degrees(45*_dash_direction.y*player.direction) 
+			charged_hitbox.set_rotation_degrees(45*_dash_direction.y*player.direction)
+			#player.particles.set_rotation_degrees(45*_dash_direction.y*player.direction)
 	
 	if !player.is_on_floor():
 		player.started_dash_in_air = true
 	player.sound_machine.play_sound("Dash", false)
 	# todo: change player hitbox so player can deal damage while dashing
-	
-	# Tilt in _dash_direction (Attention to crouch and dash)
-	if not (_dash_direction.y > 0.5 and player.is_on_floor()):
-		player.sprite.set_rotation_degrees(45*_dash_direction.y*player.direction) 
-		charged_hitbox.set_rotation_degrees(45*_dash_direction.y*player.direction)
-		#player.particles.set_rotation_degrees(45*_dash_direction.y*player.direction)
-	
 	#particleSystemPlayer.play("Dash")
+
+
+## For charged Attack: Set velocity later
+func set_velocity():
+	player.velocity = _dash_direction * player.dash_speed * 0.8 # Make Charged Dash a little slower but longer
 
 
 func exit():
 	.exit()
 	animationPlayer.set_speed_scale(1)
-	player.in_charged_attack = false
+	if player.in_charged_attack:
+		player.in_charged_attack = false
+		remove_child(charged_timer)
+		charged_timer.queue_free()
 	_jumped = false
 	player.hitbox.get_child(0).disabled = false
 	player.start_dash_cooldown()
@@ -81,20 +100,21 @@ func physics_update(delta):
 		player.dash_move(delta, _dash_direction, player.friction_dash)
 	
 	# Check for exit conditions
-	if Input.is_action_just_pressed("attack"):
-		if Input.is_action_pressed("move_up"):
-			state_machine.transition_to("Attack_Up_Windup")
-		else:
-			 state_machine.transition_to("Attack_Basic_Windup")
-	elif Input.is_action_just_pressed("block") and player.is_on_floor():
-		state_machine.transition_to("Block_Windup")
-	elif Input.is_action_just_pressed("jump") and not _jumped and player.is_on_floor():
-		timer.start(player.leap_jump_time)
-		player.hitbox.get_child(0).disabled = false
-		_jumped = true
-		player.velocity.y = -player.jump_impulse
-	elif Input.is_action_just_pressed("move_down"):
-		state_machine.transition_to("Crouch")
+	if !player.in_charged_attack:
+		if Input.is_action_just_pressed("attack"):
+			if Input.is_action_pressed("move_up"):
+				state_machine.transition_to("Attack_Up_Windup")
+			else:
+				 state_machine.transition_to("Attack_Basic_Windup")
+		elif Input.is_action_just_pressed("block") and player.is_on_floor():
+			state_machine.transition_to("Block_Windup")
+		elif Input.is_action_just_pressed("jump") and not _jumped and player.is_on_floor():
+			timer.start(player.leap_jump_time)
+			player.hitbox.get_child(0).disabled = false
+			_jumped = true
+			player.velocity.y = -player.jump_impulse
+		elif Input.is_action_just_pressed("move_down"):
+			state_machine.transition_to("Crouch")
 
 
 func _on_timeout() -> void:
