749527160cb7b02ad28b097a3d61d2e1506be0cf
Jonas Seidl <js408@hdm-stuttgart.de>
2022-01-22 20:10:31 +0100

    Changed that camera knows on which barrier the player entered and plays animation accordingly

--- Nodes/Game Objects/Fightzone.gd
+++ Nodes/Game Objects/Fightzone.gd
@@ -13,6 +13,7 @@ var right_border_pos
 var enemies = []
 
 var player = null
+var entering_barrier = 0 # 0 = left_barrier, 1 = right_barrier
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,7 +21,7 @@ func _ready():
 	var children = get_children()
 	for child in children:
 		if child.name.find("Barrier") != -1:
-			child.connect("body_exited",self,"_on_Fightzone_exited")
+			child.connect("body_exited",self,"_on_Fightzone_exited", [child])
 			child.get_node("AnimationPlayer").connect("animation_started", self, "_on_Animation_finished")
 			
 		# replace by proper condition e.g. is Enemy!!
@@ -41,11 +42,14 @@ func _process(delta):
 	#If no enemy remains inside fightzone despawn barriers
 	elif is_active and enemies.empty():
 		is_active = false
+		if entering_barrier == 0:
+			player.camera.animate_to(right_barrier.global_position)
+		elif entering_barrier == 1:
+			player.camera.animate_to(left_barrier.global_position)
+		yield(player.camera, "animation_finished")
 		right_barrier.despawn()
 		right_barrier.get_node("Border/CollisionShape2D").set_deferred("disabled", true)
 		right_barrier.get_node("DamageEmitter/CollisionShape2D2").set_deferred("disabled", true)
-		player.camera.animate_to(left_barrier.global_position)
-		yield(player.camera, "animation_finished")
 		left_barrier.despawn()
 		left_barrier.get_node("Border/CollisionShape2D").set_deferred("disabled", true)
 		left_barrier.get_node("DamageEmitter/CollisionShape2D2").set_deferred("disabled", true)
@@ -53,16 +57,29 @@ func _process(delta):
 		yield(get_tree().create_timer(despawn_delay), "timeout")
 		get_tree().paused = false
 		player.camera.reset_animate_position()
-		MusicController.fade_out_music("OST_Hectic")
+		MusicController.fade_out_music("OST_Hectic", 0.2)
 		MusicController.fade_in_at_random("OST_Ominous")
 
 ## Will be notified by Barriers if Player passed
 ## Activates and spwans Barriers if player is still inside zone after certain delay
-func _on_Fightzone_exited(body):
+func _on_Fightzone_exited(body, emitter):
 	if not is_active and body.name == "Player" and _is_within_borders(body.global_position.x) and not enemies.empty():
 		
 		if player == null:
 			player = body
+		
+		var first_barrier
+		var second_barrier
+		
+		if emitter.name == "LeftBarrier":
+			entering_barrier = 0
+			first_barrier = left_barrier
+			second_barrier = right_barrier
+		elif emitter.name == "RightBarrier":
+			entering_barrier = 1
+			first_barrier = right_barrier
+			second_barrier = left_barrier
+			
 			
 		is_active = true
 		#Delay barrier activation
@@ -73,17 +90,17 @@ func _on_Fightzone_exited(body):
 			get_tree().paused = true
 			MusicController.stop_everything(["Ambience_Atmosphere"])
 			
-			player.camera.animate_to(right_barrier.global_position)
+			player.camera.animate_to(first_barrier.global_position)
 			yield(player.camera, "animation_finished")
-			right_barrier.spawn()
-			right_barrier.sound_machine.play_sound("Spawn", false)
-			yield(right_barrier.sound_machine, "sound_finished")
+			first_barrier.spawn()
+			first_barrier.sound_machine.play_sound("Spawn", false)
+			yield(first_barrier.sound_machine, "sound_finished")
 			
-			player.camera.animate_to(left_barrier.global_position)
+			player.camera.animate_to(second_barrier.global_position)
 			yield(player.camera, "animation_finished")
-			left_barrier.spawn()
-			left_barrier.sound_machine.play_sound("Spawn", false)
-			yield(left_barrier.sound_machine, "sound_finished")
+			second_barrier.spawn()
+			second_barrier.sound_machine.play_sound("Spawn", false)
+			yield(second_barrier.sound_machine, "sound_finished")
 			
 			right_barrier.get_node("Border").set_collision_mask_bit(0,true)
 			left_barrier.get_node("Border").set_collision_mask_bit(0,true)
